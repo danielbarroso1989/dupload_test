28fdc59c089b9bf73bf6d5ca55451a0e35242c71
daniel <dbarroso@identitymind.com>
2019-08-15 15:25:16 -0500

    upda promise

--- poc/static/poc/js/app.js
+++ poc/static/poc/js/app.js
@@ -1,46 +1,68 @@
 
 //url request
 //const url = 'https://postman-echo.com/post';
-const url = "http://localhost:8000/endpoint/"
+const url = "http://c7bb828c.ngrok.io/poc-forklift/validate"
 document.addEventListener('DOMContentLoaded', init);
 
 function init(){
-    document.getElementById('btnSubmit').addEventListener('click', upload);
+    document.getElementById('btnSubmit').addEventListener('click', validate_id);
 }
 
 function upload(ev){
-    ev.preventDefault();    //stop the form submitting
+ //stop the form submitting
 
-    //create any headers we want
+
+
+}
+
+ async function validate_id(ev){
+    ev.preventDefault();   
+    var prueba = await create_id()
+    console.log(prueba)
+    console.log("prueba")
+    //validate format
+    //validate size
+    //validate null
+}
+function create_id(){
+
+
+       return new Promise(resolve => {
+
+            //create any headers we want
     let h = new Headers();
     h.append('Accept', 'application/json'); //what we expect back
     //bundle the files and data we want to send to the server
+   //h.append('pIY_KEY', 'PIROUORIUFIOR'); //what we expect back
     let fd = new FormData();
-    fd.append('user-id', document.getElementById('user_id').value);
- 
+  
     let myFile = document.getElementById('file_test').files[0];
     
 
-    fd.append('csv_file', myFile, "poc.csv");
+    fd.append('file', myFile);
     let req = new Request(url, {
         method: 'POST',
         headers: h,
-        mode: 'no-cors',
         body: fd
     });
 
 
     fetch(req)
         .then( (response)=>{
-            document.getElementById('output').textContent = "Response received from server";
+              response.json().then(function(data) {
+
+                  document.getElementById('output').textContent = `response id ${data['processId']}`;
+                 console.log(data);
+                  resolve(data['processId']);
+                });
+          
         })
         .catch( (err) =>{
             console.log('ERROR:', err.message);
         });
+
+
+    });
 }
 
-function validate_file(file){
-    //validate format
-    //validate size
-    //validate null
-}
+
