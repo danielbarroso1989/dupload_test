7140f1e81fc26d2cb63697a26d7fe0f4fd76849b
daniel <dbarroso@identitymind.com>
2019-08-16 12:55:10 -0500

    orientado

--- poc/static/poc/js/app.js
+++ poc/static/poc/js/app.js
@@ -1,97 +1,107 @@
-const url = "http://c7bb828c.ngrok.io/poc-forklift/validate";
+const url = "http://localhost:8080/poc-forklift/validate";
 
 const url2 = "http://localhost:8080/poc-forklift/process/";
 
-document.addEventListener('DOMContentLoaded', init);
+class Poc{
 
-function init() {
-    document.getElementById('btnSubmit').addEventListener('click', upload);
-}
+    constructor(){
 
-async function upload(ev) {
+        this.processId
 
-    ev.preventDefault();
+        //btn events
 
-    var prueba = await create_validate_id();
+        document.getElementById('btnSubmit').addEventListener('click', (e)=>{
 
-    console.log("prueba " + prueba);
-
-    var process_status = await query_process(prueba);
-
-    do {
-        setTimeout(async function () {
-            process_status = await query_process(prueba);
-            console.log("do", process_status['finished']);
-        }, 5);
-    } while(process_status['finished'] == 'false');
-
-
-
-}
-
-function query_process(process_id) {
-
-    return new Promise(resolve => {
-
-        let h = new Headers();
-
-        h.append('Accept', 'application/json');
-
-        let process_url = `${url2}${process_id}`;
-
-        let req = new Request(process_url, {
-            method: 'GET',
-            headers: h,
+            this.ValidateFormatDoc(e)
+            
         });
 
-        fetch(req)
-            .then((response) => {
 
-                resolve(response.json());
+        document.getElementById('seul').addEventListener('click', (e)=>{
 
-            })
-            .catch((err) => {
-                console.log('ERROR:', err.message);
+            this.prueba_test()
+            
+        });
+
+       
+    }
+
+    init_process(){
+        
+        console.log("init proceso")
+ 
+
+    }
+
+     upload_document(ev){
+
+        ev.preventDefault();
+        console.log("pruebaaaaaa")
+        prueba();
+      
+    }
+
+    create_validate_id(){
+
+        return new Promise(resolve => {
+
+            let h = new Headers();
+    
+            h.append('Accept', 'application/json');
+    
+            let fd = new FormData();
+    
+            let myFile = document.getElementById('file_test').files[0];
+    
+            fd.append('file', myFile);
+    
+            let req = new Request(url, {
+                method: 'POST',
+                headers: h,
+                body: fd
             });
-
-    });
-
-
-}
-
-function create_validate_id() {
-
-    return new Promise(resolve => {
-
-        let h = new Headers();
-
-        h.append('Accept', 'application/json');
-
-        let fd = new FormData();
-
-        let myFile = document.getElementById('file_test').files[0];
-
-        fd.append('file', myFile);
-
-        let req = new Request(url, {
-            method: 'POST',
-            headers: h,
-            body: fd
-        });
-
-        fetch(req)
-            .then((response) => {
-                response.json().then(function (data) {
-
-                    document.getElementById('output').textContent = `response id ${data['processId']}`;
-
-                    resolve(data['processId']);
+    
+            fetch(req)
+                .then((response) => {
+                    response.json().then(function (data) {
+    
+                        document.getElementById('output').textContent = `response id ${data['processId']}`;
+                    
+                        resolve(data['processId']);
+                    });
+    
+                })
+                .catch((err) => {
+                    console.log('ERROR:', err.message);
                 });
+    
+        });
 
-            })
-            .catch((err) => {
-                console.log('ERROR:', err.message);
-            });
+    }
 
-    });
+
+    async ValidateFormatDoc(e){
+
+        e.preventDefault();
+
+        this.processId = await this.create_validate_id()
+        
+        console.log(this.processId)
+    }
+
+    prueba_test(){
+     
+        console.log("huevos",this.processId)
+    }
+ 
 }
+
+
+
+const Poc_functions = new Poc()
+
+Poc_functions.init_process()
+
+
+
+
